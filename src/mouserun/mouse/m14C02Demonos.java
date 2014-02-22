24dabdbdf046b37da065d02279415eceea7cc124
Fasgort <Fasgort@gmail.com>
2014-02-22 18:01:56 +0100

    Algoritmos de busqueda

--- src/mouserun/mouse/m14C02Demonos.java
+++ src/mouserun/mouse/m14C02Demonos.java
@@ -78,121 +78,14 @@ public class m14C02Demonos extends Mouse {
             countMove++;
         }
         
-        if (currentGrid.getX() - cheese.getX() >= 0) {
-            direccionX = 3;
-        } else {
-            direccionX = 4;
+        if(maze.containsKey(new Pair<Integer, Integer> (cheese.getX(), cheese.getY()))){
+           //Algoritmo de busqueda del queso
+            return 1;
         }
-        distanciaX = Math.abs(currentGrid.getX() - cheese.getX());
-        if (currentGrid.getY() - cheese.getY() >= 0) {
-            direccionY = 2;
-        } else {
-            direccionY = 1;
+        else{
+            //Algoritmo de exploración
+            return 1;
         }
-        distanciaY = Math.abs(currentGrid.getY() - cheese.getY());
-
-        switch (lastMove) {
-            case 1:
-                lastMove = 2;
-                break;
-            case 2:
-                lastMove = 1;
-                break;
-            case 3:
-                lastMove = 4;
-                break;
-            case 4:
-                lastMove = 3;
-                break;
-        }
-
-        if (distanciaX >= distanciaY) {
-            switch (direccionX) {
-                case 3:
-                    if (currentGrid.canGoLeft() && lastMove != 3) {
-                        lastMove = 3;
-                        return Mouse.LEFT;
-                    }
-                    break;
-                case 4:
-                    if (currentGrid.canGoRight() && lastMove != 4) {
-                        lastMove = 4;
-                        return Mouse.RIGHT;
-                    }
-                    break;
-            }
-            switch (direccionY) {
-                case 1:
-                    if (currentGrid.canGoUp() && lastMove != 1) {
-                        lastMove = 1;
-                        return Mouse.UP;
-                    }
-                    break;
-                case 2:
-                    if (currentGrid.canGoDown() && lastMove != 2) {
-                        lastMove = 2;
-                        return Mouse.DOWN;
-                    }
-                    break;
-            }
-        } else {
-            switch (direccionY) {
-                case 1:
-                    if (currentGrid.canGoUp() && lastMove != 1) {
-                        lastMove = 1;
-                        return Mouse.UP;
-                    }
-                    break;
-                case 2:
-                    if (currentGrid.canGoDown() && lastMove != 2) {
-                        lastMove = 2;
-                        return Mouse.DOWN;
-                    }
-                    break;
-            }
-            switch (direccionX) {
-                case 3:
-                    if (currentGrid.canGoLeft() && lastMove != 3) {
-                        lastMove = 3;
-                        return Mouse.LEFT;
-                    }
-                    break;
-                case 4:
-                    if (currentGrid.canGoRight() && lastMove != 4) {
-                        lastMove = 4;
-                        return Mouse.RIGHT;
-                    }
-                    break;
-            }
-        }
-
-        Random random = new Random();
-        ArrayList<Integer> possibleMoves = new ArrayList<Integer>();
-
-        if (currentGrid.canGoUp()) {
-            possibleMoves.add(Mouse.UP);
-        }
-        if (currentGrid.canGoDown()) {
-            possibleMoves.add(Mouse.DOWN);
-        }
-        if (currentGrid.canGoLeft()) {
-            possibleMoves.add(Mouse.LEFT);
-        }
-        if (currentGrid.canGoRight()) {
-            possibleMoves.add(Mouse.RIGHT);
-        }
-
-        if (possibleMoves.size() == 1) {
-            lastMove = possibleMoves.get(0);
-            return possibleMoves.get(0);
-        } else {
-            do {
-                nextMove = possibleMoves.get(random.nextInt(possibleMoves.size()));
-            } while (nextMove == lastMove);
-            lastMove = nextMove;
-            return lastMove;
-        }
-
     }
 
     public void newCheese() {
